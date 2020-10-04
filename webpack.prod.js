bd7d8b886132bdc371ea4e1a72e0b2c41477e22f
Data Camp256 <68553874+datacamp256@users.noreply.github.com>
2020-10-04 20:45:25 +0200

    feature: L4-5 - prod optimizations

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,29 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    // rules: [
-    //     {
-    //         test: /\.scss$/,
-    //         use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-    //     }
-    // ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+        ]
+    },
     plugins: [
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
     ],
     output: {
         libraryTarget: 'var',
         library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
 });

b67a61f92c7b7246bead41170af5fc7c24391cf1
Data Camp256 <68553874+datacamp256@users.noreply.github.com>
2020-10-05 11:51:56 +0200

    chore: rearrange css files #3

--- src/client/index.js
+++ src/client/index.js
@@ -5,10 +5,11 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import './styles/main.scss'
 
 console.log(checkForName);
 
-alert("I EXIST")
+// alert("I EXIST")
 console.log("CHANGE!!");
 export {
     checkForName,

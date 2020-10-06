d693ca3b6e56880d3fac002dcfd3bc2ecceacf7d
Data Camp256 <68553874+datacamp256@users.noreply.github.com>
2020-10-07 00:11:04 +0200

    chore: remove unused library
    #4

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,4 @@
-// let path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
 const apiKeyLoader = require('./apiKeyLoader')
 const meaningCloudClient = require('./meaningCloudClient')
 
@@ -19,10 +17,6 @@ app.get('/', function (req, res) {
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
 })
 
 app.get('/meaningcloud', async function (req, res) {

150543155aa1202713c6b1aab69e3d68fcd5b757
Data Camp256 <68553874+datacamp256@users.noreply.github.com>
2020-10-07 10:03:39 +0200

    feature: show error if connection to the server is faulty
    #5

--- src/client/js/__test__/formHandler.test.js
+++ src/client/js/__test__/formHandler.test.js
@@ -3,10 +3,12 @@ import {describe, expect} from '@jest/globals';
 import fetchMock from "fetch-mock";
 
 
-
 describe('In "formHandler"', () => {
     describe('the function "handleSubmit"', () => {
         let defaultWasPrevented;
+        const eventMock = {
+            preventDefault: preventDefaultCallback()
+        };
 
         function preventDefaultCallback() {
             return jest.fn(() => defaultWasPrevented = true);
@@ -14,15 +16,25 @@ describe('In "formHandler"', () => {
 
         beforeEach(() => {
             defaultWasPrevented = false;
+            fetchMock.restore();
         })
 
         test('should call the API', function () {
             document.body.innerHTML = `<input id="name" type="::type::" name="" value="::text::">`
             const responseBody = {hello: "world"};
             fetchMock.getOnce('http://localhost:8081/meaningcloud?text=::text::', responseBody);
-            const eventMock = {
-                preventDefault: preventDefaultCallback()
-            };
+
+            formHandler.handleSubmit(eventMock);
+
+            expect(defaultWasPrevented).toBeTruthy();
+        });
+
+        test('should show an error if connection is malfunctios', function () {
+            document.body.innerHTML = `<form class="" >
+<input id="name" type="::type::" name="" value="::text::">
+<label id="connection-error" style="display: none;" class="error-label">::error-text::</label>
+</form>`
+            fetchMock.getOnce('http://localhost:8081/meaningcloud?text=::text::', 400);
 
             formHandler.handleSubmit(eventMock);
 
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,34 @@
+const VISIBLE_ERROR = 'display: block;';
+const INVISIBLE_ERROR = 'display: none;';
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-
-    fetch('http://localhost:8081/meaningcloud?text='+encodeURI(formText))
-    .then(res => res.json())
-    .then(function(res) {
-        Client.update(res)
-    });
+    toggleErrorLabel(false);
+    fetch('http://localhost:8081/meaningcloud?text=' + encodeURI(formText))
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error(`Failed to connect to server with ${res.status}`);
+            }
+        })
+        .then(function (res) {
+            Client.update(res)
+        })
+        .catch(error => {
+            console.log(error);
+            toggleErrorLabel(true);
+        });
 }
 
+function toggleErrorLabel(setVisible) {
+    const errorLabel = document.getElementById('connection-error');
+    const errorIsVisible = errorLabel.getAttribute('style') === VISIBLE_ERROR;
+    if (setVisible !== errorIsVisible) {
+        errorLabel.setAttribute('style',setVisible ? VISIBLE_ERROR : INVISIBLE_ERROR);
+    }
+}
 module.exports.handleSubmit = handleSubmit;
